4c65065c93bcda6e19972144458b731466810738
Tipok <tipok@trueaxiongames.com>
2019-03-12 10:17:12 +0700

    rename folder

--- export.py
+++ export.py
@@ -1,13 +1,26 @@
 import shutil
+import os
 
-def CopyDirectory() :
-	print("Start copy Directory.")
+MOD_NAME = "MomoTweak"
+MOD_DIRECTORY = "\Mod"
+IS_ZIP = False
+
+def zipdir(dir_name, output_filename):
+        if (IS_ZIP):
+                shutil.make_archive(output_filename, 'zip', dir_name)
+
+def CopyDirectory(version):
+        path = os.path.realpath(__file__)
+        directory = os.path.dirname(path)
+        print("Start zipping Directory at " + directory + MOD_DIRECTORY)
+        zipdir(directory, MOD_NAME + "_" + version)
+def GetVersion():
+        version = "0"
+        
+        return version
 
 print("Init Exporting.")
-CopyDirectory()
+CopyDirectory(GetVersion())
 
 print(input())
 
-#!/usr/bin/env python
-def zipdir(dir_name, output_filename):
-    shutil.make_archive(output_filename, 'zip', dir_name)
