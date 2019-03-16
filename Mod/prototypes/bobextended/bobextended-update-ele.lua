232c07d181d3f93f4c9f59a2bda7dd82ccef0304
FinalGuessing - MomoSundeass <final.guessing@gmail.com>
2019-03-17 00:36:22 +0700

    add speed factor

--- Mod/prototypes/bobextended/bobextended-update-ele.lua
+++ Mod/prototypes/bobextended/bobextended-update-ele.lua
@@ -4,6 +4,7 @@ local ELE = momoTweak.ele
 local MEM = momoTweak.ele.memory
 local CON = momoTweak.ele.controller
 local JUN = momoTweak.ele.junction
+local SPEEDFACTOR = 2
 local category = data.raw.recipe[ELE.unit[2]].category
 
 local function result(name, amount)
@@ -37,7 +38,7 @@ momoTweak.createRecipe(category,
 	{
 		{ELE.cable[1], 2},
 		{"nickel-plate", 1}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.board[2])
+	}, 2 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.board[2])
 )
 
 bobmods.lib.recipe.add_ingredient(ELE.unit[1], {MEM.a, 2})
@@ -50,7 +51,7 @@ momoTweak.createRecipe(category,
 		{ELE.comp[1], 3},
 		{MEM.a, 4},
 		{"silicon-wafer", 2}
-	}, 8, momoTweak.get_tech_of_recipe(ELE.unit[3])
+	}, 8 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[3])
 )
 bobmods.lib.recipe.remove_ingredient(ELE.circuit[3], "copper-plate")
 bobmods.lib.recipe.add_ingredient(ELE.circuit[3], {MEM.a2, 2})
@@ -65,7 +66,7 @@ momoTweak.createRecipe(category,
 	    {ELE.cable[5], 4},
 		{"silicon-nitride", 4},
 		{"cpy", 1}
-	}, 10, momoTweak.get_tech_of_recipe(ELE.unit[4])
+	}, 10 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[4])
 )
 
 bobmods.lib.recipe.add_ingredient(ELE.unit[4], {MEM.a3, 8})
@@ -77,7 +78,7 @@ momoTweak.createRecipe(category,
 		{ELE.comp[1], 4},
 		{ELE.comp[2], 2},
 		{"silver-plate", 1}
-	}, 4, momoTweak.get_tech_of_recipe(ELE.unit[2])
+	}, 4 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[2])
 )
 bobmods.lib.recipe.add_ingredient(ELE.unit[2], {MEM.b, 2})
 
@@ -89,7 +90,7 @@ momoTweak.createRecipe(category,
 		{ELE.board[2], 2},
 		{ELE.comp[3], 4},
 		{"gold-plate", 1}
-	}, 8, momoTweak.get_tech_of_recipe(ELE.unit[3])
+	}, 8 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[3])
 )
 
 bobmods.lib.recipe.remove_ingredient(ELE.unit[3], ELE.comp[1])
@@ -117,7 +118,7 @@ momoTweak.createRecipe(category,
 		{ELE.circuit[1], 1},
 		{"tin-plate", 4},
 		{"solder", 2}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.circuit[2])
+	}, 2 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.circuit[2])
 )
 
 momoTweak.replace_with_ingredient(ELE.circuit[2], "tin-plate", {CON.a, 1})
@@ -131,7 +132,7 @@ momoTweak.createRecipe(category,
 		{ELE.comp[1], 12},
 		{"silicon-wafer", 2},
 		{"solder", 2}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.unit[2])
+	}, 3 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[2])
 )
 momoTweak.replace_with_ingredient(ELE.unit[2], ELE.comp[1], {CON.b, 2})
 
@@ -143,7 +144,7 @@ momoTweak.createRecipe(category,
 		{ELE.cable[2], 4},
 		{"gold-plate", 4},
 		{"solder", 2}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.unit[3])
+	}, 4 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[3])
 )
 momoTweak.replace_with_ingredient(ELE.circuit[4], "gold-plate", {CON.c, 1})
 momoTweak.replace_with_ingredient(ELE.unit[3], ELE.comp[2], {CON.c, 1})
@@ -157,7 +158,7 @@ momoTweak.createRecipe(category,
 		{"silicon-wafer", 6},
 		{"silicon-nitride", 2},
 		{"solder", 4}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.unit[4])
+	}, 4 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[4])
 )
 momoTweak.replace_with_ingredient(ELE.unit[4], ELE.comp[3], {CON.d, 1})
 -- -------------------------------------------------------------------------------------
@@ -177,7 +178,7 @@ momoTweak.createRecipe(category,
 		{"silicon-wafer", 3},
 		{"gold-plate", 2},
 		{"solder", 2}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.unit[3])
+	}, 6 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[3])
 )
 momoTweak.replace_with_ingredient(ELE.unit[3], ELE.comp[3], {JUN.a, 1})
 
@@ -190,6 +191,6 @@ momoTweak.createRecipe(category,
 		{"silicon-nitride", 2},
 	    {ELE.cable[4], 4},
 		{"solder", 4}
-	}, 2, momoTweak.get_tech_of_recipe(ELE.unit[4])
+	}, 8 * SPEEDFACTOR, momoTweak.get_tech_of_recipe(ELE.unit[4])
 )
 momoTweak.replace_with_ingredient(ELE.unit[4], ELE.comp[4], {JUN.b, 1})
