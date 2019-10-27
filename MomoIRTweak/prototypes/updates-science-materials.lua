fff7941b9185e119ed6299cf059d174c6c4c4cca
FinalGuessing - MomoSundeass <final.guessing@gmail.com>
2019-10-27 16:57:50 +0700

    vial now use glass ingot

--- MomoIRTweak/prototypes/updates-science-materials.lua
+++ MomoIRTweak/prototypes/updates-science-materials.lua
@@ -1,5 +1,5 @@
 local recipe = momoIRTweak.NewRecipe("smelting", "momo-science-vial", 3, {
-	momoIRTweak.FastItem("sand", 5)
+	momoIRTweak.FastItem("glass-ingot", 5)
 }, 26.6)
 recipe.enabled = true
 recipe.subgroup = momoIRTweak.science.materialSubgroup
