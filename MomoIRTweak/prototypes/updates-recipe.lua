960143f20175d7975325358581cc4aeee124befe
FinalGuessing - MomoSundeass <final.guessing@gmail.com>
2019-11-13 22:28:50 +0700

    uncomment recipe balanced

--- MomoIRTweak/prototypes/updates-recipe.lua
+++ MomoIRTweak/prototypes/updates-recipe.lua
@@ -10,37 +10,50 @@ local NewPrototype = momoIRTweak.recipe.BuildPrototype
 local tech = momoIRTweak.technology
 
 function momoIRTweak.updates.WoodToCokeRecipe()
-	local unlockTech = data.raw.technology[tech.FindFromRecipe("k-coke")]
-	
+	local unlockTech = data.raw.technology["advanced-oil-processing"]
+
 	-- wood 2 coke
-	-- local woodToCoke = NewRecipePrefix("coke-smelting", "k-coke", 20, {
-		-- ITEM("wood", 24)
-	-- }, 12)
-	-- woodToCoke.subgroup = "non-handcrafting"
-	-- tech.AddUnlockEffect(unlockTech.name, woodToCoke.name)
+	local woodToCoke = NewRecipePrefix("coke-smelting", "k-coke", 8, {
+		ITEM("wood", 16)
+	}, 12)
+	woodToCoke.subgroup = "non-handcrafting"
+	tech.AddUnlockEffect(unlockTech.name, woodToCoke.name)
 	
 	local tintTable = momoIRTweak.recipe.GetCraftingMachineTint("light-oil-cracking")
 	
 	local function NewOilRecipe(category, subgroup, name, ingredients, times, results)
-		local toOil = NewPrototype(category, "coal", 1, ingredients, times)
+		local toOil = NewPrototype(name, category, "coal", 1, ingredients, times)
 		toOil.subgroup = subgroup
 		momoIRTweak.recipe.ChangePrototypeResults(toOil, name, results)
-		-- data:extend({toOil})
+		-- toOil.enabled = true
+		data:extend({toOil})
 		tech.AddUnlockEffect(unlockTech.name, toOil.name)
 		momoIRTweak.recipe.SetCraftingMachineTint(toOil.name, tintTable)
 	end
 	
 	
 	-- -- rubber wood to oil
-	-- NewOilRecipe("chemistry", "fluid-recipes", "sapling-2-oil", {ITEM("sapling", 20)}, 20, {Fluid("light-oil", 20)}) 
+	NewOilRecipe("chemistry", "fluid-recipes", "sapling-2-oil", {
+		ITEM("wood-sapling", 10)
+	}, 6, {
+		Fluid("light-oil", 36)
+	}) 
 	
 	
-	-- -- -- rubber wood to oil
-	-- NewOilRecipe("chemistry", "fluid-recipes", "rubber-wood-2-oil", {ITEM("rubber-wood", 20)}, 20, {Fluid("light-oil", 80)}) 
+	-- -- rubber wood to oil
+	NewOilRecipe("chemistry", "fluid-recipes", "rubber-wood-2-oil", {
+		ITEM("rubber-wood", 10)
+	}, 12, {
+		Fluid("light-oil", 96)
+	}) 
 	
 	
-	-- -- -- sapling to oil
-	-- NewOilRecipe("chemistry", "fluid-recipes", "rubber-sapling-2-oil", {ITEM("rubber-sapling", 20)}, 20, {Fluid("light-oil", 40)}) 
+	-- -- sapling to oil
+	NewOilRecipe("chemistry", "fluid-recipes", "rubber-sapling-2-oil", 
+		{ITEM("rubber-sapling", 10)
+	}, 6, {
+		Fluid("light-oil", 54)
+	}) 
 	
 	tech.AddUnitCount(unlockTech.name, 40)
 end
