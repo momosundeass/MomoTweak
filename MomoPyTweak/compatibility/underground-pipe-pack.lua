78f98e92c0d89fd5aa51cc23edcb5ae1de10d5a3
LAPTOP <final.guessing@gmail.com>
2023-03-06 13:54:07 +0700

    py improve underground pipe recipes

--- MomoPyTweak/compatibility/underground-pipe-pack.lua
+++ MomoPyTweak/compatibility/underground-pipe-pack.lua
@@ -5,59 +5,34 @@ local Rem = momoIRTweak.recipe.RemoveIngredient
 local Replace = momoIRTweak.recipe.ReplaceIngredient
 local MergeSubGroup = momoIRTweak.subgroups.MergeSubGroup
 
-function momoPyTweak.compatibility.PipePack()
-	momoIRTweak.technology.SetPrerequire("advanced-underground-piping", {"py-storage-tanks"})
+local function ReplaceIngredientFor(recipe, removeNames, addItem)
+	for _, name in pairs(removeNames) do
+		Rem(recipe, name)
+	end
 	
-	data.raw.item["small-pipe-coupler"].stack_size = 500
-	data.raw.item["medium-pipe-coupler"].stack_size = 500
-	data.raw.item["large-pipe-coupler"].stack_size = 500
-	
-	data.raw.item["underground-pipe-segment-t1"].stack_size = 500
-	data.raw.item["underground-pipe-segment-t2"].stack_size = 500
-	data.raw.item["underground-pipe-segment-t3"].stack_size = 500
-	
-	momoIRTweak.recipe.ReplaceIngredient("pipe-to-ground", "iron-plate", ITEM("stone-brick", 5))
-	
-	--- tier 2
-	AddIng("medium-pipe-coupler", ITEM("aluminium-plate"))
-	AddIng("medium-pipe-coupler", ITEM("lead-plate"))
-	Rem("medium-pipe-coupler", "iron-plate")
-	
-	AddIng("underground-pipe-segment-t2", ITEM("aluminium-plate", 1))
-	AddIng("underground-pipe-segment-t2", ITEM("lead-plate", 1))
-	Rem("underground-pipe-segment-t2", "iron-plate")
-	
-	
-	--- tier 3
-	AddIng("large-pipe-coupler", ITEM("niobium-plate", 1))
-	AddIng("large-pipe-coupler", ITEM("plastic-bar", 2))
-	AddIng("large-pipe-coupler", ITEM("rubber", 2))
-	Rem("large-pipe-coupler", "steel-plate")
-	
-	AddIng("underground-pipe-segment-t3", ITEM("niobium-plate", 1))
-	AddIng("underground-pipe-segment-t3", ITEM("plastic-bar", 2))
-	AddIng("underground-pipe-segment-t3", ITEM("rubber", 2))
-	Rem("underground-pipe-segment-t3", "steel-plate")
-	
-	
-	momoIRTweak.ChangeGroup("advanced-underground-piping", "logistics")
-	
-	
+	AddIng(recipe, addItem)
+end
 
-	MergeSubGroup({
-		"one-to-one-forward-pipe", "one-to-two-perpendicular-pipe", "one-to-three-forward-pipe", "one-to-four-pipe",
-		"underground-i-pipe", "underground-L-pipe", "underground-t-pipe", "underground-cross-pipe", 
-		"4-to-4-pipe"}, 
-	"pipes-to-ground")
+function momoPyTweak.compatibility.PipePack()	
+	local t1Removes = {"underground-pipe-segment-t1", "pipe"}
+	ReplaceIngredientFor("one-to-one-forward-pipe", t1Removes, ITEM("pipe-to-ground", 1))
+	ReplaceIngredientFor("one-to-two-perpendicular-pipe", t1Removes, ITEM("pipe-to-ground", 2))
+	ReplaceIngredientFor("one-to-three-forward-pipe", t1Removes, ITEM("pipe-to-ground", 3))
+	ReplaceIngredientFor("one-to-four-pipe", t1Removes, ITEM("pipe-to-ground", 4))
 	
-	MergeSubGroup({
-		"one-to-one-forward-t2-pipe", "one-to-two-perpendicular-t2-pipe", "one-to-three-forward-t2-pipe", "one-to-four-t2-pipe",
-		"underground-i-t2-pipe", "underground-L-t2-pipe", "underground-t-t2-pipe", "underground-cross-t2-pipe"}, 
-	"pipes-to-ground-t2")
+	momoIRTweak.item.ValidateItem("niobium-pipe-to-ground", function (item)
+		local t2Removes = {"underground-pipe-segment-t2", "pipe"}
+		ReplaceIngredientFor("one-to-one-forward-t2-pipe", t2Removes, ITEM("niobium-pipe-to-ground", 1))
+		ReplaceIngredientFor("one-to-two-perpendicular-t2-pipe", t2Removes, ITEM("niobium-pipe-to-ground", 2))
+		ReplaceIngredientFor("one-to-three-forward-t2-pipe", t2Removes, ITEM("niobium-pipe-to-ground", 3))
+		ReplaceIngredientFor("one-to-four-t2-pipe", t2Removes, ITEM("niobium-pipe-to-ground", 4))
+	end)
 	
-	MergeSubGroup({
-		"one-to-one-forward-t3-pipe", "one-to-two-perpendicular-t3-pipe", "one-to-three-forward-t3-pipe", "one-to-four-t3-pipe",
-		"underground-i-t3-pipe", "underground-L-t3-pipe", "underground-t-t3-pipe", "underground-cross-t3-pipe"},
-	"pipes-to-ground-t3")
-	
+	momoIRTweak.item.ValidateItem("ht-pipes-to-ground", function (item)
+		local t3Removes = {"underground-pipe-segment-t3", "pipe"}
+		ReplaceIngredientFor("one-to-one-forward-t3-pipe", t3Removes, ITEM("ht-pipes-to-ground", 1))
+		ReplaceIngredientFor("one-to-two-perpendicular-t3-pipe", t3Removes, ITEM("ht-pipes-to-ground", 2))
+		ReplaceIngredientFor("one-to-three-forward-t3-pipe", t3Removes, ITEM("ht-pipes-to-ground", 3))
+		ReplaceIngredientFor("one-to-four-t3-pipe", t3Removes, ITEM("ht-pipes-to-ground", 4))
+	end)
 end
